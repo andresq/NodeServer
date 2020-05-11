50aec3a8633a8a96f056655f2a7d9d315e39c871
Andres Quintero <hello@andresq.com>
2020-05-10 22:40:00 -0400

    updates gitignore for /node_modules

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 */
 
 const http = require('http');
+const Jimp = require('jimp');
 const fs = require('fs');
 
 const port = 2700;
@@ -10,41 +11,55 @@ const server = http.createServer();
 
 
 
+Jimp.read('assets/o.png', (err, image) => {
+    if (err) {
+          throw err;
+      }
+      image
+          .resize(128, 128)   // resize
+          .greyscale()        // set greyscale
+          .color([
+              {apply: "darken", params: [10]},
+              {apply: "red", params: [90]},
+              {apply: "green", params: [193]},
+              {apply: "blue", params: [103]}	//applies blue coloring to greyscale img
+          ])
+          .write('newO.png', () => console.log("File Saved!") );
+  });
+
+// // Routing
 
 
-// Routing
+// const routing_handler = function (req, res){
+//     console.log(`Getting a request for ${req.url} from ${req.socket.remoteAddress}`);
 
+//     // console.log(req);
 
-const routing_handler = function (req, res){
-    console.log(`Getting a request for ${req.url} from ${req.socket.remoteAddress}`);
-
-    // console.log(req);
-
-    if(req.url === '/'){
-        const main = fs.createReadStream('html/main.html');
-        res.writeHead(200, {'Content-Type':'text/html'});
-        main.pipe(res);
-    }
-    else if(req.url.startsWith('/css/')){
-        const css = fs.createReadStream('css/main.css');
-        res.writeHead(200, {'Content-Type':'text/css'});
-        css.pipe(res);
-    }
-    else{
-        const error404 = fs.createReadStream('html/404.html');
-        res.writeHead(404, {'Content-Type':'text/html'});
-        error404.pipe(res);
-    }
-}
+//     if(req.url === '/'){
+//         const main = fs.createReadStream('html/main.html');
+//         res.writeHead(200, {'Content-Type':'text/html'});
+//         main.pipe(res);
+//     }
+//     else if(req.url.startsWith('/css/')){
+//         const css = fs.createReadStream('css/main.css');
+//         res.writeHead(200, {'Content-Type':'text/css'});
+//         css.pipe(res);
+//     }
+//     else{
+//         const error404 = fs.createReadStream('html/404.html');
+//         res.writeHead(404, {'Content-Type':'text/html'});
+//         error404.pipe(res);
+//     }
+// }
 
 
 
 
-const listening_handler = function(){
-    console.log(`Now listening on PORT: ${port}`);
-}
-// Running the server
-server.listen(port);
-server.on('listening', listening_handler);
+// const listening_handler = function(){
+//     console.log(`Now listening on PORT: ${port}`);
+// }
+// // Running the server
+// server.listen(port);
+// server.on('listening', listening_handler);
 
-server.on('request', routing_handler);
+// server.on('request', routing_handler);
